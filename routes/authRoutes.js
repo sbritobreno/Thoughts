2aecb51a7d12e800a545baf66654d3ad94e04bd0
Breno Brito <breno.silvabrito@hotmail.com>
2022-06-29 23:14:20 +0100

    Create login form

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,5 +6,7 @@ const ThoughtController = require('../controllers/AuthController')
 router.get('/login', AuthController.login)
 router.get('/register', AuthController.register)
 router.post('/register', AuthController.registerPost)
+router.get('/logout', AuthController.logout)
+
 
 module.exports = router
