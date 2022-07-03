6f29e6cc376ffb5e854e43866ba178faba9e8d46
Breno Brito <breno.silvabrito@hotmail.com>
2022-07-03 18:50:35 +0100

    Create function for search bar

--- controllers/ThoughtController.js
+++ controllers/ThoughtController.js
@@ -1,15 +1,33 @@
 const Thought = require('../models/Thought')
 const User = require('../models/User')
 
+const { Op } = require('sequelize')
+
 module.exports = class ThoughtController {
 
     static async showThoughts(req, res) {
+
+        let search = ''
+        if(req.query.search){
+            search = req.query.search
+        }
+
         const thoughtsData = await Thought.findAll({
             include: User,
+            where: {
+                title: { [Op.like]: `%${search}%`},
+            },
         })
         const thoughts = thoughtsData.map((result) => result.get({plain: true}) )
 
-        res.render('thoughts/home', { thoughts })
+        let thoughtQty = thoughts.length
+        console.log(thoughtQty)
+
+        if (thoughtQty === 0){
+            thoughtQty = false
+        }
+
+        res.render('thoughts/home', { thoughts, search, thoughtQty })
     }
 
     static async dashboard(req, res){
